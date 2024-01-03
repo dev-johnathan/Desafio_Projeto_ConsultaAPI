91471463ac3f4cbf3985748d805f8d11b9a10069
Lyandra Fernandes Cardoso <lyandrafercar2014@gmail.com>
2024-01-03 17:07:16 -0300

    docs: adição de comentários na pasta src

--- src/server.js
+++ src/server.js
@@ -1,16 +1,17 @@
-// importação de biblioteca e criação de instancia do express
+// importação de biblioteca e criação de instancia
 const express = require('express');
 const app = express();
 const path = require('path');
 const apiChuckNorrisRoute = require('./routes/apiChuckNorrisRoute');
 const apiAtividadesRoute = require('./routes/apiAtividadeRoute');
-// definição de porta para iniciaar o servidor
+// definição de porta para iniciar o servidor
 const port = 3000;
 
+// execução de middleware das rotas
 app.use('/', apiChuckNorrisRoute);
 app.use('/', apiAtividadesRoute);
 
-// servindo arquivos estáticos da pasta 'public'
+// execução de middleware de arquivos estáticos da pasta 'public'
 app.use('/public', express.static(path.join(__dirname, './views','public')));
 
 // rota padrão que carrega o index.html
